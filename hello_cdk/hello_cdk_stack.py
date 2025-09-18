26a31e89490a00b798abf32ab6f7900e71786172
ferrindsouza <55425975+ferrindsouza@users.noreply.github.com>
2025-09-18 13:04:07 +0530

    initial lambda to display HelloWorld

--- hello_cdk/hello_cdk_stack.py
+++ hello_cdk/hello_cdk_stack.py
@@ -1,19 +1,35 @@
 from aws_cdk import (
-    # Duration,
     Stack,
-    # aws_sqs as sqs,
-)
+    aws_lambda as _lambda, # Import the Lambda module
+    CfnOutput, # Import CfnOutput
+    )
 from constructs import Construct
 
 class HelloCdkStack(Stack):
 
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
+        # Define Lambda inside _init_
+        my_function = _lambda.Function(
+            self, "HelloWorldFunction",
+            runtime=_lambda.Runtime.NODEJS_20_X,  # Provide any supported Node.js runtime
+            handler="index.handler",
+            code=_lambda.Code.from_inline(
+                """
+                exports.handler = async function(event) {
+                  return {
+                    statusCode: 200,
+                    body: JSON.stringify('Hello World!'),
+                  };
+                };
+                """
+            ),
+        )
 
-        # The code that defines your stack goes here
+        # Add a Function URL
+        my_function_url = my_function.add_function_url(
+                auth_type=_lambda.FunctionUrlAuthType.NONE,
+        )
 
-        # example resource
-        # queue = sqs.Queue(
-        #     self, "HelloCdkQueue",
-        #     visibility_timeout=Duration.seconds(300),
-        # )
+        # Output the URL to CloudFormation
+        CfnOutput(self,"myFunctionUrlOutput",value=my_function_url.url,)
